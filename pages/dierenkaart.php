279ed1b24ec7543159ee86fce60450d57c617f31
Vincent007super <115561924+Vincent007super@users.noreply.github.com>
2024-04-05 13:44:10 +0200

    LANG LEVE DE PHP
    Jij krijgt een php bestand, en jij krijgt een php bestand. IEDEREEN KRIJGT EEN PHP BESTAND!!!

--- pages/dierenkaart.php
+++ pages/dierenkaart.php
@@ -1,14 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dierenasiel</title>
     <link rel="stylesheet" href="../style/Dierenkaart.css">
     <link rel="stylesheet" href="../style/site.css">
-</head>
 
 <body>
     <header>
