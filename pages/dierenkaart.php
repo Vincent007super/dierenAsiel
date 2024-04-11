b13f136b52e027d8b5160b4d5fd4dedb0724d2e1
Vincent007super <vrhoogenraat11@gmail.com>
2024-04-11 12:26:21 +0200

    Database can be loaded into website
    Only 'ras' works
    Lay-out has yet to work
    map with all the 'dierenkaarten' yet to be filled with pages

--- pages/dierenkaart.php
+++ pages/dierenkaart.php
@@ -1,32 +1,50 @@
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Dierenasiel</title>
-<link rel="stylesheet" href="../style/Dierenkaart.css">
-<link rel="stylesheet" href="../style/site.css">
+<?php
+$servername = "localhost";
+$username = "88695";
+$password = "Shailesh2006";
+$dbname = "Pootjesparadijs";
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}   
+
+$sql = "SELECT * FROM Dierenkaart"; // Assuming 'Dierenkaart' is the name of your table
+$result = $conn->query($sql);
+
+$data = array();
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+} else {
+    echo "No data found";
+}
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dierenasiel</title>
+    <link rel="stylesheet" href="../style/Dierenkaart.css">
+    <link rel="stylesheet" href="../style/site.css">
+</head>
 <body>
-<header>
-        <div class="wrapper">
-            <a href="../index.php" id="logo_header">
-            <img src="../media/img/Logo.png" alt="Logo Pootjesparadijs" id="logo">
+    <header>
+        <div id="buttons">
+            <a href="login.html">
+                <div id="log" class="header_bttn">
+                    <span class="span_buttons">Inloggen</span>
+                </div>
             </a>
-            <div id="menuWrapper">
-            <a href="../index.php">Hoofdpagina</a>
-                        <a href="about_us.html">Over Ons</a>
-                        <a class="active" href="dierenkaart.php">Dierenkaart</a>
-                        <a href="klantenkaart.php">Klantenkaart</a>
-            </div>
-            <div id="buttons">
-                <a href="login.php">
-                    <div id="log" class="header_bttn">
-                        <span class="span_buttons">Inloggen</span>
-                    </div>
-                </a>
-                <div onclick="doneer()">
-                    <div id="don" class="header_bttn">
-                        <span class="span_buttons">Doneren</span>
-                    </div>
+            <div onclick="doneer()">
+                <div id="don" class="header_bttn">
+                    <span class="span_buttons">Doneren</span>
                 </div>
             </div>
         </div>
@@ -39,275 +57,46 @@
             </div>
         </div>
     </div>
-    </div>
-
     <main>
         <div id="menu"></div>
         <div id="selectie">
-            <div class="wrapper2">
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
+            <?php
+            foreach ($data as $item) {
+                ?>
+                <div class="wrapper2">
+                    <div class="hyper">
+                        <div class="overlay"></div>
+                        <a class="dierLink">
+                            <img class="dierFoto">
+                            <div class="wrapper3">
+                                <h3 class="dierNaam"><?php echo $item['Naam']; ?></h3>
+                                <p class="dierRas"><?php echo $item['Ras']; ?></p>
+                                <p class="dierInfo"><?php echo $item['Info']; ?></p>
+                            </div>
+                        </a>
+                    </div>
                 </div>
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="wrapper2">
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="wrapper2">
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="wrapper2">
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="wrapper2">
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-                <div class="hyper">
-                    <div class="overlay"></div>
-                    <a class="dierLink">
-                        <img class="dierFoto">
-                        <div class="wrapper3">
-                            <h3 class="dierNaam"></h3>
-                            <p class="dierRas"></p>
-                            <p class="dierInfo"></p>
-                        </div>
-                    </a>
-                </div>
-            </div>
+                <?php
+            }
+            ?>
         </div>
     </main>
-
     <div id="footer">
         <div class="footer_links">
             <a class="policy" href="policy.html">Privacy Policy</a>
             <a class="terms" href="terms.html">Terms & Conditions</a>
-            <!--- Voeg hier meer links toe naarmate het nodig is --->
         </div>
         <div class="filler3"></div>
         <div class="footer_cont" id="footer_main2">
             <div class="footer_img">
-                <a href="https://www.postcodeloterij.nl/goede-doelen/overzicht/dierenbescherming" target="_blank">
+                <a href="https://www.postcodeloterij.nl" target="_blank">
                     <img src="../media/img/PostcodeLoterij.png" alt="PostcodeLoterij" width="64px" height="42px">
                 </a>
-                <a href="https://www.playstation.com/en-us/" target="_blank">
-                    <img src="../media/img/Playstation.png" alt="Playstation" width="60px" height="42px">
-                </a>
-                <a href="https://www.bp.com/" target="_blank">
-                    <img src="../media/img/bp.png" alt="Shell" width="60px" height="80px">
-                </a>
-                <a href="https://www.remarms.com/" target="_blank">
-                    <img src="../media/img/REM-L.png" alt="Shell" width="150px" height="80px">
-                </a>
-                <a href="https://gta.fandom.com/wiki/Los_Santos_Customs" target="_blank">
-                    <img src="../media/img/LosSantosCustoms.webp" alt="Shell" width="150px" height="80px">
-                </a>
+                <!-- Add other footer links -->
             </div>
         </div>
         <div class="filler3"></div>
     </div>
-
-    </div>
     <script src="../script/doneer.js"></script>
 </body>
-
 </html>
