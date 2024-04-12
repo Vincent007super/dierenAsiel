17fef6eaad02b4d3a70947873aac5cdcfe0277b9
Shailesh770 <skramkisoen@gmail.com>
2024-04-12 10:13:34 +0200

    test

--- pages/klantenkaart.php
+++ pages/klantenkaart.php
@@ -61,7 +61,9 @@
             <input type="number" name="legimatieNummer" id="legimatieNummer" required placeholder="BSN nummer"
                 maxlength="9" minlength="9"
                 value="<?php echo isset($_POST['legimatieNummer']) ? $_POST['legimatieNummer'] : ''; ?>" readonly>
-            <p>Het wachtwoord kan niet worden ingeladen vanwege privacy...</p>
+                <p>Wachtwoord</p>
+            <input type="password" name="wachtwoord" id="wachtwoord" required placeholder="wachtwoord123"
+                autocomplete="on" minlength="4" maxlength="15" readonly>
             <input type="hidden" name="submitted" value="true">
     </div>
     </form>
--- pages/login.php
+++ pages/login.php
@@ -105,7 +105,35 @@ if (isset($_POST['submitted'])) {
 
 
             </html>
-            <!--Footer hier-->
+            
+    <div id="footer">
+        <div class="footer_links">
+            <a class="policy" href="pages/policy.html">Privacy Policy</a>
+            <a class="terms" href="pages/terms.html">Terms & Conditions</a>
+            <!--- Voeg hier meer links toe naarmate het nodig is --->
+        </div>
+        <div class="filler3"></div>
+        <div class="footer_cont" id="footer_main2">
+            <div class="footer_img">
+                <a href="https://www.postcodeloterij.nl/goede-doelen/overzicht/dierenbescherming" target="_blank">
+                    <img src="media/img/PostcodeLoterij.png" alt="PostcodeLoterij" width="64px" height="42px">
+                </a>
+                <a href="https://www.playstation.com/en-us/" target="_blank">
+                    <img src="media/img/Playstation.png" alt="Playstation" width="60px" height="42px">
+                </a>
+                <a href="https://www.bp.com/" target="_blank">
+                    <img src="media/img/bp.png" alt="Shell" width="60px" height="80px">
+                </a>
+                <a href="https://www.remarms.com/" target="_blank">
+                    <img src="media/img/REM-L.png" alt="Shell" width="150px" height="80px">
+                </a>
+                <a href="https://gta.fandom.com/wiki/Los_Santos_Customs" target="_blank">
+                    <img src="media/img/LosSantosCustoms.webp" alt="Shell" width="150px" height="80px">
+                </a>
+            </div>
+        </div>
+        <div class="filler3"></div>
+    </div>
             <script src="../script/doneer.js"></script>
         </body>
 
